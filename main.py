298bc50a79fa47fbbdc6af38c91753956f48a041
Marks Meskis <mark@MacBook-Air-Marks.local>
2026-07-14 22:41:05 +0300

    Updated app version 0.3

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def health():
 def info():
     return {
         "service": "mini-platform",
-        "version": "0.2",
+        "version": "0.3",
         "hostname": socket.gethostname()
     }
 
